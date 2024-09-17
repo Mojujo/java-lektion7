aeaf1410cd76a6b43f114e14590ac67f4fb55bd2
Mojujo <oscar.tornqvist01@gmail.com>
2024-09-17 12:12:40 +0200

    Final

--- src/main/java/se/oscar/Calculator.java
+++ src/main/java/se/oscar/Calculator.java
@@ -1,17 +1,7 @@
 package se.oscar;
 
-public class CalculatorFelExercise {
-    public static void main(String[] args) {
-        System.out.println(division(10,0));
-    }
-
-    public static int division(int a, int b) {
-        int sum = 0;
-        try {
-            sum = a / b;
-        } catch (ArithmeticException e) {
-            System.out.println("Kan inte dividera med noll");
-        }
-        return sum;
+public class Calculator {
+    public int divide(int a, int b) throws ArithmeticException {
+        return a/b;
     }
 }
--- src/main/java/se/oscar/Main.java
+++ src/main/java/se/oscar/Main.java
@@ -1,4 +1,13 @@
 package se.oscar;
 
 public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        try {
+            System.out.println(calculator.divide(10,0));
+        } catch (ArithmeticException e) {
+            System.out.println("Cant divine by zero");
+        }
+    }
 }
